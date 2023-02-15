f1c509bad6f8f6ae1c15ced7f8bd9ca69da884af
Arslan Baig <arn3870@gmail.com>
2023-02-16 02:36:38 +0500

    json server added

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
+import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/Tasks';
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService){}
+
+  ngOnInit(): void {
+    this.taskService.getTask().subscribe((tasks) => (this.tasks = tasks
+    ));
+  }
 }
