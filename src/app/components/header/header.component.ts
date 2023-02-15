6e1c40c9739b21da51b467892b48f782eac32f99
Arslan Baig <arn3870@gmail.com>
2023-02-15 17:56:01 +0500

    base cards setup

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title = 'angular-todo';
+  toggleAddTask = () => {
+    console.log('toggle')
+  }
+}
